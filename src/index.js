4ae92238840dc24311ce61cf73d7433bfe5f96ce
Italo Gustavo <itamelo555@gmail.com>
2024-12-31 00:22:24 -0300

    feat: add error handler

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
+app.use((error, request, response) => {
+  if (error) {
+    console.log(error)
+    return response.status(500).json({ error: 'Internal server error' })
+  }
+})
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 })
