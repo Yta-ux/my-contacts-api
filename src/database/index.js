b70bdb29548a48f186c66f242af218687d989f86
Italo Gustavo <itamelo555@gmail.com>
2025-02-21 00:00:36 -0300

    wip

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,9 @@ const { Client } = require('pg')
 const client = new Client({
   host: process.env("PGHOST"),
   port: process.env("PGPORT"),
-  user: process.env("PGPORT"),
-  password: process.env("PGPORT"),
-  database: process.env("DATPGDATABASEABASE")
+  user: process.env("PGUSER"),
+  password: process.env("PGPASSWORD"),
+  database: process.env("PGDATABASE")
 })
 
 client.connect()
