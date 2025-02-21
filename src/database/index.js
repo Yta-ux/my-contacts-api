8110095e056c59a236be77e956cbd9f0a6b30f6b
Italo Gustavo <itamelo555@gmail.com>
2025-02-21 00:17:40 -0300

    wip

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg')
 
 const client = new Client({
-  host: process.env("PGHOST"),
-  port: process.env("PGPORT"),
-  user: process.env("PGUSER"),
-  password: process.env("PGPASSWORD"),
-  database: process.env("PGDATABASE")
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE
 })
 
 client.connect()
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use((error, request, response) => {
   }
 })
 
-const PORT = process.env("PORT") || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('Server running on port 3000');
 })
